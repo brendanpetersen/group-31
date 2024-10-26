30fc355fc5653fb4e6483189f81217fea65a9e8b
brendanpetersen <n9700480@qut.edu.au>
2024-10-26 11:30:32 +1000

    commit

--- error.py
+++
@@ -1,5 +0,0 @@
-# Error 404 
-"404 Not Found - Sorry, the requested page could not be found but may be available again in the future."
-
-# Error 500
-"500 Internal Server Error - Sorry unable to find server." 
