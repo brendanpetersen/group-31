2a32b207833b87378577a6b971441f78703ff2c2
Matthew McKern <mattymckern@hotmail.com>
2024-11-01 12:13:41 +1000

    created show.html and updated models.py and concert.py to allow for dynamic content

--- concert/upcoming_concerts.py
+++ concert/upcoming_concerts.py
@@ -6,17 +6,20 @@ eventbp = Blueprint('upcoming', __name__, url_prefix='/events')
 
 @eventbp.route('/<id>')
 def show(id):
-    return render_template('events/show.html')
+    return render_template('events/show.html', concert=Concert)
 
 def get_concert():
-  b_desc = """American musician with a blend of country and hip-hop; 'Start a Riot' was included in Spider-Man: Into the Spider-Verse."""
-  image_loc = 'img/shaboozey.webp'
-  Concert = Concert('Shaboozey', b_desc,image_loc, 'The Triffid, Newstead', '21/11/2024 20:00', '$120')
-  # a comment
-  comment = Comment("Sam", "I've booked already!", '2024-08-12 11:00:00')
-  Concert.set_comments(comment)
-  comment = Comment("Juliette", "purr slay queen", '2024-08-12 11:00:00')
-  Concert.set_comments(comment)
-  comment = Comment("Sally", "hurray", '2024-08-12 11:00:00')
-  Concert.set_comments(comment)
-  return Concert
+    b_desc = """American musician with a blend of country and hip-hop; 'Start a Riot' was included in Spider-Man: Into the Spider-Verse."""
+    image_loc = 'static/img/shaboozey.webp'  # Adjusted to include the static folder
+    concert = Concert('Shaboozey', b_desc, 'The Triffid, Newstead', '21/11/2024', '20:00', image_loc, '$120')
+    
+    # Set comments
+    comments = [
+        Comment("Sam", "I've booked already!", '2024-08-12 11:00:00'),
+        Comment("Juliette", "purr slay queen", '2024-08-12 11:00:00'),
+        Comment("Sally", "hurray", '2024-08-12 11:00:00')
+    ]
+    for comment in comments:
+        concert.set_comments(comment)
+    
+    return concert
