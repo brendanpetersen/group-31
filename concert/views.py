4c6f7313a5f2c877f596dae13179d9dab51f2ae3
Matthew McKern <mattymckern@hotmail.com>
2024-11-01 21:35:32 +1000

    created dynamic events to display on index card from database

--- concert/views.py
+++ concert/views.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Concert
 from . import db
-
 
 mainbp = Blueprint('main', __name__)
 
@@ -10,7 +9,18 @@ def index():
     home = db.session.scalars(db.select(Concert)).all()    
     return render_template('index.html', concert=home)
 
-@mainbp.route('/bookings', methods = ['GET', 'POST'])
+@mainbp.route('/bookings', methods=['GET', 'POST'])
 def bookings():
     return render_template('bookings.html')
+
+@mainbp.route('/search')
+def search():
+    search_term = request.args.get('search', '')  # Use .get() to avoid KeyError
+    if search_term:
+        print(search_term)
+        query = f"%{search_term}%"
+        concert_search = db.session.scalars(db.select(Concert).where(Concert.description.like(query))).all()
+        return render_template('index.html', concert=concert_search)
+    else:
+        return redirect(url_for('main.index'))
 
