bc864553474fbb2a86022e1dd8c7ba13d9597972
Matthew McKern <mattymckern@hotmail.com>
2024-11-01 19:34:46 +1000

    created sql database

--- concert/__init__.py
+++ concert/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -8,7 +8,10 @@ db = SQLAlchemy()
 def create_app(template_folder='templates'):
     template_folder_path = os.path.join(os.path.dirname(__file__), '..', 'templates')
     print("Creating app with template folder:", template_folder)
+    
     app = Flask(__name__, template_folder=template_folder_path)
+
+    # Register Blueprints
     from . import views
     app.register_blueprint(views.mainbp)
 
@@ -18,21 +21,19 @@ def create_app(template_folder='templates'):
     from . import auth
     app.register_blueprint(auth.authbp)
 
-    Bootstrap5(app)
+    # Initialize Bootstrap
+    bootstrap = Bootstrap(app)  # Only use this line
 
     app.secret_key = 'pecanpie'
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///traveldb.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///concertdb.sqlite'
     db.init_app(app)
 
-    @app.errorhandler(404) 
-    # inbuilt function which takes error as parameter 
-    def not_found(e): 
-      return render_template("404.html", error=e)
+    @app.errorhandler(404)
+    def not_found(e):
+        return render_template("404.html", error=e)
 
-    @app.errorhandler(500) 
-    # inbuilt function which takes error as parameter 
-    def not_found(e): 
-      return render_template("500.html", error=e)
+    @app.errorhandler(500)
+    def server_error(e):
+        return render_template("500.html", error=e)
+
     return app
-
--- concert/models.py
+++ concert/models.py
@@ -1,6 +1,17 @@
 from . import db
 from datetime import datetime
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    comments = db.relationship('Comment', backref='user')
+    
+    def __repr__(self):
+        return f"Name: {self.name}"
+
 class Concert(db.Model):
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
